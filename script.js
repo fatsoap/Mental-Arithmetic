5af5dd4c1cf2452f0d9f90527da824f475bf482d
fatsoap <g8sigma@icloud.com>
2022-03-02 14:57:33 +0800

    fix onload not work

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
-let input_field = document.getElementById('input_field');
-let question_field = document.getElementById('question_field');
-let timer = document.getElementById('timer');
-let correct_amount = document.getElementById('correct_amount');
-let total_amount = document.getElementById('total_amount');
-let pause = true;
-
-input_field.addEventListener('keyup', function (e) {
-  e.preventDefault();
-  console.log(e.keyCode);
-  if (e.keyCode === 13) {
-    submit();
-  } else if (e.keyCode === 32) {
-    pause = !pause;
-  }
-});
+let input_field;
+let question_field;
+let timer;
+let correct_amount;
+let total_amount;
+let pause;
 
 function init() {
   question_field.innerHTML = '';
@@ -22,6 +12,7 @@ function init() {
     li.innerText = Math.floor(Math.random() * 200 - 100);
     question_field.appendChild(li);
   }
+  input_field.value = '';
 }
 
 function submit() {
@@ -33,22 +24,61 @@ function submit() {
   }
   let correct_flag = document.getElementById('correct_flag');
   if (total === ans) {
-    correct_flag.style.backgroundColor = '#81ff81';
+    correct_flag.style.backgroundColor = '#70ca70';
     correct_amount.innerText = Number(correct_amount.innerText) + 1;
   } else {
     correct_flag.style.backgroundColor = '#fc7e7e';
   }
   total_amount.innerText = Number(total_amount.innerText) + 1;
   correct_flag.innerText = total;
-  input_field.innerText = '';
   init();
 }
 
-init();
-input_field.focus();
-pause = false;
-setInterval(() => {
-  if (!pause) {
-    timer.innerText = Number(timer.innerText) + 1;
-  }
-}, 1000);
+function togglePause() {
+  pause = !pause;
+  document.getElementById('pause_cover').style.height = pause ? '100%' : '0%';
+}
+
+function restart() {
+  init();
+  timer.innerText = '0';
+  total_amount.innerText = '0';
+  correct_amount.innerText = '0';
+  correct_flag.innerText = '';
+  correct_flag.style.backgroundColor = '#5f5f5f';
+}
+
+window.onload = function () {
+  input_field = document.getElementById('input_field');
+  question_field = document.getElementById('question_field');
+  timer = document.getElementById('timer');
+  correct_amount = document.getElementById('correct_amount');
+  total_amount = document.getElementById('total_amount');
+  pause = true;
+  input_field.addEventListener('keyup', function (e) {
+    e.preventDefault();
+    console.log(e.keyCode);
+    if (e.keyCode === 13) {
+      // enter
+      submit();
+    }
+  });
+  document.querySelector('body').addEventListener('keyup', function (e) {
+    e.preventDefault();
+    if (e.keyCode === 32) {
+      // space
+      togglePause();
+    } else if (e.keyCode === 82) {
+      //r
+      restart();
+    }
+  });
+  init();
+  input_field.focus();
+  togglePause();
+  setInterval(() => {
+    if (!pause) {
+      timer.innerText = Number(timer.innerText) + 1;
+    }
+  }, 1000);
+};
